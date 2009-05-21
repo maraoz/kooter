075a0f50781ea5dc278983bd14885ac87173fed7
williams.matias <williams.matias@a768d256-45a6-11de-82b9-170f4fd74ac7>
2009-05-21 03:14:04 +0000

    Commit inicial con todas las carpetas y todo

---
+++ trunk/src/kernel.c
@@ -0,0 +1,66 @@
+#include "../include/kasm.h"
+#include "../include/defs.h"
+
+DESCR_INT idt[0x80];			/* IDT de 128 entradas*/
+IDTR idtr;				/* IDTR */
+
+int tickpos=640;
+
+void int_08() {
+
+    char *video = (char *) 0xb8000;
+    video[tickpos+=2]='*';
+
+}
+
+/**********************************************
+kmain() 
+Punto de entrada de cóo C.
+*************************************************/
+
+kmain() 
+{
+
+        int i,num;
+
+/* Borra la pantalla. */ 
+
+	k_clear_screen();
+
+
+/* CARGA DE IDT CON LA RUTINA DE ATENCION DE IRQ0    */
+
+        setup_IDT_entry (&idt[0x08], 0x08, (dword)&_int_08_hand, ACS_INT, 0);
+	
+
+/* CARGA DE IDT CON LA RUTINA DE ATENCION DE write()    */
+
+        setup_IDT_entry (&idt[0x80], 0x10, (dword)&_int_80_hand, ACS_INT, 0);
+
+/* CARGA DE IDT CON LA RUTINA DE ATENCION DE read()    */
+
+        setup_IDT_entry (&idt[0x80], 0x08, (dword)&_int_80_hand, ACS_INT, 0);
+
+
+/* Carga de IDTR    */
+
+	idtr.base = 0;  
+	idtr.base +=(dword) &idt;
+	idtr.limit = sizeof(idt)-1;
+	
+	_lidt (&idtr);	
+
+	_Cli();
+/* Habilito interrupcion de timer tick*/
+
+        _mascaraPIC1(0xFE);
+        _mascaraPIC2(0xFF);
+        
+	_Sti();
+
+        while(1)
+        {
+        }
+	
+}
+

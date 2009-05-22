0f7a39644ca495e0d5b707984296eac7c283ba6d
williams.matias <williams.matias@a768d256-45a6-11de-82b9-170f4fd74ac7>
2009-05-22 23:13:16 +0000

    Termine sys_write (aunque van a surgir 89000 problemas) Voy por sys_read ahora :D

--- trunk/src/libasm.asm
+++ trunk/src/libasm.asm
@@ -61,32 +61,43 @@ _int_08_hand:				; Handler de INT 8 ( Timer tick)
         pusha                           ; Carga de DS y ES con el valor del selector
         mov     ax, 10h			; a utilizar.
         mov     ds, ax
-        mov     es, ax                  
-        call    int_08                 
+        mov     es, ax
+        call    int_08
         mov	al,20h			; Envio de EOI generico al PIC
 	out	20h,al
-	popa                            
+	popa
         pop     es
         pop     ds
         iret
 
 
-
+; Llama a la interrupcion 80h
+; Recibe en en el primer parametro de stack si es write o read. 
+; 0 si se quiere llamar a sys_read y 1 si se quiere llamar a sys_write. Es de tipo byte (8 bits).
+; Recibe como segundo parametro en el stack el file descriptor. Es de tipo byte (8 bits).
+; Recibe como tercer parametro de stack el offset. Es de tipo dword (32 bits).
+; Recibe como cuarto parametro de stack el dato. Es de tipo byte (8 bits).
+	
 _int_80_caller:
 	push	ebp
 	mov	ebp,esp
-
+	push	ebx
+	push	ecx
+	push	edx
 	
 	mov	ah,[ebp+8]
 	mov	bh,[ebp+12]
-	mov	cl,[ebp+16]
+	mov	ecx,[ebp+16]
+	mov	dl,[ebp+20]
 	
 	int	80h
+
+	pop	edx
+	pop	ecx
+	pop	ebx
 	mov	esp,ebp
 	pop	ebp
 	ret
-
-
 
 
 ; Manejador de rutinas de atencion para la interrupcion 80h
@@ -97,40 +108,72 @@ _int_80_caller:
 _int_80_hand:				; Handler de INT 80h (sys_read  y sys_write)
 	push	ebp
 	mov	ebp,esp
-	push	eax			; Se salvan los registros
-        push    ds
-        pusha
+	push	ebx
+	push	ecx
+	push	edx
+	push    ds
+
 	cmp	ah,0
-	jz	sys_write
+	call	sys_write
 	cmp	ah,1
-	jz	sys_read
-					; si llega aca AH no es valido
-	jmp 	int_80_end
+	call	sys_read
+
+        pop     ds
+	pop	edx
+	pop	ecx
+	pop	ebx
+	mov	esp,ebp
+	pop 	ebp
+	iret
 	
 sys_write:
+	cmp	bh,0
+	jz	pantalla		; si el file descriptor es 0 (pantalla) voy a escribir a a B8000h
+	cmp	bh,3
+	jz	wmemoria			; si el file descriptor es 3 (memoria) voy a escribir a a 0CAFEh
+	ret
+	
 
-	cmp	bh,0			; si el file descriptor no es 0 (pantalla)
-	jnz	int_80_end		; me voy de la funcion (es el unico dispositivo
-					; que se puede escribir).
+wmemoria:
+	mov	ax,10h			
+	mov	ds,ax
+	mov	ebx,0CAFEh
+	add	ebx,ecx
+	mov	[ds:ebx],dl			; Copio en la posicion de memoria el char a escribir
+        ret
+		
 
-	mov 	ch,  07h
+pantalla:
+
+	mov 	dh,  07h			; cargo el formato (letras blancas) en que se escribe cada byte en pantalla
 
 	mov	ax,10h			
 	mov	ds,ax
 	mov	ebx,0B8000h
-	mov	[ds:ebx],cx			; Copio en la posicion de memoria el char a escribir
-    jmp	int_80_end
+	add	ebx,ecx
+	mov	[ds:ebx],dx			; Copio en la posicion de memoria el char a escribir
+        ret
 
 sys_read:
+	cmp	bh,1
+	jz	mouse		; si el file descriptor es 1 -> leo del mouse
+	cmp	bh,2		
+	jz	teclado		; si el file descriptor es 2 -> leo del teclado
+	cmp	bh,3
+	jz	rmemoria		; si el file descriptor es 3 -> leo de la memoria
+	ret
 	
-      
-int_80_end:
-	popa
-        pop     ds
-	pop	eax
-	mov	esp,ebp
-	pop 	ebp
-	iret
+mouse:
+	ret
+teclado:
+	ret
+
+rmemoria:
+	mov	ax,10h			
+	mov	ds,ax
+	mov	eax,[ds:ecx]			; Copio de la posicion de memoria que hay en ecx a eax
+	ret
+
 
 
 ; Debug para el BOCHS, detiene la ejecució; Para continuar colocar en el BOCHSDBG: set $eax=0

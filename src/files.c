7991fa2016de13d5652ff809d9f4f4b4485bb933
williams.matias <williams.matias@a768d256-45a6-11de-82b9-170f4fd74ac7>
2009-10-04 23:15:51 +0000

    Hice la logica (falta chequearla) sobre los files y directorios suponiendo que los directorios son tags

---
+++ src/files.c
@@ -0,0 +1,196 @@
+
+
+
+#define MAX_QTY_FILES 1000
+#define MAX_QTY_TAGS 31 /* es la cantidad de bits disponibles */
+dword cwd = 0;
+
+typedef struct {
+    files_t file;
+    boolean used = FALSE;
+    int references = 0; /* referencias a este fd */
+} files_entry;
+
+typedef struct {
+    char name[100];
+    char * data;
+    int index = 0;
+    dword tags; /* los tags van a ser bits */
+} files_t;
+
+files_entry opened_files[MAX_QTY_FILES];
+char tag_list[MAX_QTY_TAGS][100];
+
+/*
+ * Funcion que crea un archivo en caso de no existir
+ */
+
+int
+open(char * name){
+    int index;
+    files_t file;
+    strncpy(file.name,name,100);
+    file.tags = get_actual_tags(); /* esto esta mal pero cuando definamos la funcion get_actual_tags() lo corrijo */
+    file.data = malloc(100); /* numero magico, poner una valor mejor */
+    index = get_next_file_entry();
+    if(index == -1){
+        return -1;
+    }
+    open_files[index].file = file;
+    open_files[index].used = TRUE;
+    open_files[index].references++;
+    return i;
+}
+
+/*
+ * Funcion que cierra un archivo abierto
+ */
+int
+close(int index){
+    if(!is_valid_fd(index) || opened_files[index] == 0 || opened_files[index].used == FALSE)
+        return -1;
+    opened_files[index]--;
+    return index;
+}
+
+
+/*
+ * Funcion que lee un archivo abierto
+ * Hay que ver si mergeamos las dos funciones de write y read que tenemos (la de stdio y la de files)
+ */
+int
+read(char * name,char * buffer){
+    int i;
+    int index;
+    index = get_fd(name);
+    if(!is_valid_fd(index) || opened_files[index].used == FALSE)
+        return -1;
+    for(i=0;i<MAX_BUFFER_SIZE && opened_files[index]!=0;i++){
+       buffer[i] = opened_files[index].file.data[i];
+    }
+}
+
+/*
+ * Funcion que escribe un archivo un archivo abierto
+ */
+int
+write(char * name, char * input){
+    int index;
+    index = get_fd(name);
+    if(!is_valid_fd(index) || opened_files[index].used == FALSE)
+        return -1;
+    for(i=0; i<MAX_FILE_SIZE-1 && input[i] !=0;i++){
+        opened_files[index].file.data[i] = input[i];
+    }
+    opened_files[index].file.data[i] = 0;
+    return i;
+}
+
+/*
+ * Funcion que borra un archivo
+ */
+int
+unlink(char * name){
+    int index;
+    index = get_fd(name);
+    if(index == -1)
+        return -1;
+    opened_files[index].used = FALSE;
+}
+
+/*
+ * Funcion que chequea que el fd sea valido
+ */
+boolean
+is_valid_fd(int index){
+    if(index < 0 || index > MAX_QTY_FILES)
+        return false;
+    return true;
+}
+
+/*
+ * Funcion que cambia de directorio
+ */
+int
+chdir(char * directory){
+    dword tag;
+    tag = get_numeric_tag(directory);
+    if(tag == -1){
+        return -1;
+    }
+    cwd = cwd|tag;
+}
+
+/*
+ * Funcion que lista el contenido de un directorio
+ */
+int
+ls(char * directory){
+    dword tag;
+    int i;
+    if(directory[0] == 0){
+        tag = cwd;
+    } else {
+        tag = get_numeric_tag(directory);
+    }
+    for(i = 0 ; i<MAX_QTY_FILES ; i++){
+        if(opened_files[i].used == TRUE && opened_files[i].file.tag == tag){
+            putln(opened_files[i].file.name);
+    }
+}
+
+/*
+ * Funcion que crea directorios
+ */
+int
+mkdir(char * directory){
+    dword tag;
+    int i;
+    tag = get_next_available_tag();
+    if(tag == -1){
+        return -1;
+    }
+    i = log2(tag);
+    strncopy(tag_list[i],directory,100);
+}
+
+/*
+ * Funcion que elimina directorios
+ */
+int
+rmdir(char * directory){
+    dword tag;
+    int index;
+    tag = get_numeric_tag(directory);
+    if(tag == -1)
+        return -1;
+    tag_list[log2(tag)][0]=0;
+    return tag;
+}
+
+/*
+ * Funcion que devuelve le proximo tag posible;
+ */ 
+dword
+get_next_available_tag(){
+    int i;
+    for(i = 0; i<MAX_QTY_TAGS && tag_list[i][0]!=0 ; i++);
+    if(i == MAX_QTY_TAGS){
+        return -1;
+    }
+    return 1<<i /*lo mismo que pow(2,i)*/;
+}
+
+/*
+ * Funcion que devuelve un tag numerico a partir de un nombre de directorio
+ */
+
+dword
+get_numeric_tag(char * name){
+    int i;
+    for(i = 0; i<MAX_QTY_TAGS && !strcmp(tag_list[i],name) ; i++);
+    if(i == MAX_QTY_TAGS){
+        return -1;
+    }
+    return 1<<i;
+}

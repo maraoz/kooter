47fc34c25dad256e6718fa616939e2269fef4b5e
manuelaraoz <manuelaraoz@a768d256-45a6-11de-82b9-170f4fd74ac7>
2009-10-05 15:41:19 +0000

    arreglos para q compile

--- trunk/src/allocator.c
+++ trunk/src/allocator.c
@@ -1,7 +1,14 @@
 
+#include "../include/kasm.h"
+#include "../include/defs.h"
+#include "../include/kc.h"
 
 extern void * eokl;
-char * mem = (char*)eokl + 4096;
+char * mem;
+
+void allocator_init() {
+    char * mem = (char*)eokl + 4096;
+}
 
 void * malloc(int size){
     return mem+=size;

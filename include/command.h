c7334d71cef0f3b790942d9443aab1218d9c84fa
williams.matias <williams.matias@a768d256-45a6-11de-82b9-170f4fd74ac7>
2009-10-08 15:08:40 +0000

    Nuevos intentos de hacer funcionar el tpe de so

--- include/command.h
+++ include/command.h
@@ -5,8 +5,8 @@
 
 #ifndef _command_h
 #define _command_h
-int Asd(int argc, char ** argv);
+int asd(int argc, char ** argv);
 
-int Bnm(int argc, char ** argv);
+int bnm(int argc, char ** argv);
 
 #endif
